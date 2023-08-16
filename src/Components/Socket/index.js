379adc6edb1e5d499619f6bf5dc82d51895719fc
shiva prasad <chennojushivaprasad1@gmail.com>
2023-08-16 22:24:36 +0530

    sixth commit

--- src/Components/Socket/index.js
+++ src/Components/Socket/index.js
@@ -17,9 +17,72 @@ const Socket = (props) => {
 
   const socketIoUrl = process.env.REACT_APP_BASE_URL;
 
+  const acceptCall = async (data) => {
+    // const remotePeerConnection = new RTCPeerConnection(configuration);
+
+    const { offer, senderSocketID, senderId, recipientId } = data;
+    // const configuration = {
+    //   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    // };
+
+    localPeerConnection.current.setRemoteDescription(
+      new RTCSessionDescription(offer)
+    );
+    const answer = await localPeerConnection.current.createAnswer();
+    await localPeerConnection.current.setLocalDescription(answer);
+
+    socket.emit("acceptedCall", {
+      answer,
+      senderId,
+      recipientId,
+      senderSocketID,
+    });
+  };
+
+
   useEffect(() => {
-    localPeerConnection.current = new RTCPeerConnection();
-  }, []);
+    if (incomingCallStatus) {
+      acceptCall(incomingCallDetails);
+    }
+  }, [incomingCallStatus, acceptCall, incomingCallDetails]);
+
+  const accessVideoAndAudio = async () => {
+    const openMediaDevices = async (constraints) => {
+      return await navigator.mediaDevices.getUserMedia(constraints);
+    };
+
+    try {
+      const stream = await openMediaDevices({ video: true, audio: true });
+
+      setMyStream(stream);
+    } catch (error) {
+      console.error("Error accessing media devices.", error);
+    }
+  };
+
+ 
+  const makeVideoCall = async (senderId, recipientId) => {
+    const offer = await localPeerConnection.current.createOffer();
+    await localPeerConnection.current.setLocalDescription(offer);
+    socket.emit("outgoingCall", { senderId, recipientId, offer });
+
+    socket.on("answeredCall", async (data) => {
+      const { answer } = data;
+      const remoteDescription = new RTCSessionDescription(answer);
+      await localPeerConnection.current.setRemoteDescription(remoteDescription);
+    });
+
+    localPeerConnection.current.addEventListener(
+      "connectionstatechange",
+      (event) => {
+        if (localPeerConnection.connectionState === "connected") {
+          // Peers connected!
+          console.log("peers connected");
+        }
+      }
+    );
+  };
+
 
   useEffect(() => {
     if (userId) {
@@ -53,69 +116,12 @@ const Socket = (props) => {
     }
   });
 
+
   useEffect(() => {
-    if (incomingCallStatus) {
-      acceptCall(incomingCallDetails);
-    }
-  }, [incomingCallStatus, acceptCall, incomingCallDetails]);
+    localPeerConnection.current = new RTCPeerConnection();
+  }, []);
 
-  const accessVideoAndAudio = async () => {
-    const openMediaDevices = async (constraints) => {
-      return await navigator.mediaDevices.getUserMedia(constraints);
-    };
 
-    try {
-      const stream = await openMediaDevices({ video: true, audio: true });
-
-      setMyStream(stream);
-    } catch (error) {
-      console.error("Error accessing media devices.", error);
-    }
-  };
-
-  const acceptCall = async (data) => {
-    // const remotePeerConnection = new RTCPeerConnection(configuration);
-
-    const { offer, senderSocketID, senderId, recipientId } = data;
-    // const configuration = {
-    //   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-    // };
-
-    localPeerConnection.current.setRemoteDescription(
-      new RTCSessionDescription(offer)
-    );
-    const answer = await localPeerConnection.current.createAnswer();
-    await localPeerConnection.current.setLocalDescription(answer);
-
-    socket.emit("acceptedCall", {
-      answer,
-      senderId,
-      recipientId,
-      senderSocketID,
-    });
-  };
-
-  const makeVideoCall = async (senderId, recipientId) => {
-    const offer = await localPeerConnection.current.createOffer();
-    await localPeerConnection.current.setLocalDescription(offer);
-    socket.emit("outgoingCall", { senderId, recipientId, offer });
-
-    socket.on("answeredCall", async (data) => {
-      const { answer } = data;
-      const remoteDescription = new RTCSessionDescription(answer);
-      await localPeerConnection.current.setRemoteDescription(remoteDescription);
-    });
-
-    localPeerConnection.current.addEventListener(
-      "connectionstatechange",
-      (event) => {
-        if (localPeerConnection.connectionState === "connected") {
-          // Peers connected!
-          console.log("peers connected");
-        }
-      }
-    );
-  };
 
   const values = {
     incomingCallStatus,
